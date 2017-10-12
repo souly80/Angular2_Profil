5e8069d69d7339047f6cf3af5fe762f61ee04d46
szemmouri <s.zemmouri@proway-fs.com>
2017-10-13 00:32:47 +0200

    create visit-card component

---
+++ src/app/visit-card/visit-cart.component.ts
@@ -0,0 +1,12 @@
+
+import {Component} from "@angular/core";
+
+@Component({
+    selector:'visit-cart',
+    templateUrl: './visit-cart.component.html',
+    styleUrls:['./visit-cart.style.css']
+})
+export class VisitCartComponent {
+
+}
+
