39cf0eb6c873dd07e8a4048094df2fb29c2e6c2c
szemmouri <s.zemmouri@proway-fs.com>
2017-10-14 09:16:32 +0200

    delete compiled files

--- src/app/impressum-view/impressum.service.js
+++
@@ -1,1 +0,0 @@
-//# sourceMappingURL=impressum.service.js.map
--- src/app/profil-view/PersonalData.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-var PersonalData = (function () {
-    function PersonalData() {
-    }
-    return PersonalData;
-}());
-exports.PersonalData = PersonalData;
-//# sourceMappingURL=PersonalData.js.map
