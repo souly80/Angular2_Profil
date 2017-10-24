569ae2de9a83f8c624e86949fff78facb31343f6
szemmouri <s.zemmouri@proway-fs.com>
2017-10-24 22:20:55 +0200

    add expander button

--- src/app/project-view/project-view.component.ts
+++ src/app/project-view/project-view.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { ProjectViewService } from './project-view.service';
-import { CommonUtils } from '../CommonUtils';
 import { Observable } from 'rxjs/Rx';
 import {IProjectData} from "./project-view.interface";
 
@@ -11,6 +10,7 @@ import {IProjectData} from "./project-view.interface";
   providers: [ProjectViewService],
 })
 export class ProjectViewComponent {
+  public isExpaned: boolean;
   private skills: any;
   private skill: string;
   private pointer: number = 0;
@@ -20,8 +20,13 @@ export class ProjectViewComponent {
   projectIcon: string = require('../../../public/assets/project_icon_24.png');
   projectIconMobile: string = require('../../../public/assets/project_icon_16.png');
   projectsData: IProjectData[];
+  isExpanedArray: boolean[];
   constructor(private projectViewService: ProjectViewService) {
     this.projectsData = this.projectViewService.getProjectData();
+    this.isExpanedArray = [];
+    for(let i =0; i< this.projectsData.length - 1; i++) {
+      this.isExpanedArray[i] = false;
+    }
     this.skills = ["Industrie 4.0", "IoT", "Manufacturing Execution System", "Bildverarbeitungsysteme", "Application Lifecycle Management", "..."];
     Observable.interval(2000)
       .subscribe((x) => {
@@ -31,5 +36,9 @@ export class ProjectViewComponent {
           this.pointer++;
         this.skill = this.skills[this.pointer];
       });
+  }
+
+  public onExpaned(value: boolean,index: number) {
+    this.isExpanedArray[index] = value;
   }
 } 
--- src/app/ui-components/expander/expander.component.ts
+++ src/app/ui-components/expander/expander.component.ts
@@ -1,11 +1,36 @@
-import {Component, Input} from "@angular/core";
+import {Component, EventEmitter, Input, OnChanges, Output, SimpleChanges} from "@angular/core";
 
 @Component({
     selector: 'expander-component',
     templateUrl: 'expander.component.html',
     styleUrls: ['./expander.style.css']
 })
-export class ExpanderComponent {
+export class ExpanderComponent implements OnChanges {
+
+    public itemExpaned: boolean;
+
+    @Output()
+    public isExpanded:EventEmitter<boolean>;
+
+    constructor() {
+        this.isExpanded = new EventEmitter();
+    }
+
+    public opened() {
+        //this.isExpanded.next(true);
+        this.itemExpaned = true;
+    }
+
+    public closed() {
+        //this.isExpanded.next(false);
+        this.itemExpaned = false;
+    }
+
+    ngOnChanges(changes: SimpleChanges): void {
+
+    }
+
+
     @Input()
     projectIcon:string;
     @Input()
